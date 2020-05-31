e06dce260d44ddfdfb55264bc49fb8cee7cbec89
artdsouza <64566136+artdsouza@users.noreply.github.com>
2020-05-31 14:16:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,63 +2,73 @@ import random
 from fractions import Fraction
 
 print("Welcome to the Fractions Game!")
-
+print()
+print("-" * 30)
 print("Choose your operation:")
 print("1 - Multiplication")
 print("2 - Addition")
 print("3 - Subtraction")
 print("4 - Division")
 operation = int(input("Operation:"))
-
+print("-" * 30)
+print()
 print("Choose a level:")
 print("1 - Easy")
 print("2 - Difficult")
 # ask for level
 level = int(input("Level: "))
+print("-" * 30)
+print()
 
 limit = 10
 # the difficult level;
 if level == 2:
-  limit = 100
+	limit = 100
 
 count = 0
 
 while count < 5:
-  count = count + 1
+	print()
+	count = count + 1
 
-  first_n = random.randrange(0, limit)
-  first_d = random.randrange(1, limit)
-  first = Fraction(first_n, first_d)
+	first_n = random.randrange(0, limit)
+	first_d = random.randrange(1, limit)
+	first = Fraction(first_n, first_d)
 
-  sec_n = random.randrange(0, limit)
-  sec_d = random.randrange(1, limit)
-  second = Fraction(sec_n, sec_d)
+	sec_n = random.randrange(0, limit)
+	sec_d = random.randrange(1, limit)
+	second = Fraction(sec_n, sec_d)
 
-  # if the user chooses multiplication
-  if operation == 1:
-    print(f"Calculate {first} x {second}")
-    correct = first * second
-  # if the user chooses addition
-  if operation == 2:
-    print(f"Calculate {first} + {second}")
-    correct = first + second
-    # if the user chooses subtraction
-  if operation == 3:
-    print(f"Calculate {first} - {second}")
-    correct = first - second
-    # if the user chooses division
-  if operation == 4:
-    print(f"Calculate {first} / {second}")
-    correct = first / second
+	while second.denominator == 1:
+		sec_n = random.randrange(0, limit)
+		sec_d = random.randrange(1, limit)
+		second = Fraction(sec_n, sec_d)
 
-  given = Fraction(input(f"Your Answer: "))
+	# if the user chooses multiplication
+	if operation == 1:
+		print(f"Calculate {first} x {second}")
+		correct = first * second
+	# if the user chooses addition
+	if operation == 2:
+		print(f"Calculate {first} + {second}")
+		correct = first + second
+		# if the user chooses subtraction
+	if operation == 3:
+		print(f"Calculate {first} - {second}")
+		correct = first - second
+		# if the user chooses division
+	if operation == 4:
+		print(f"Calculate {first} ÷ {second}")
+		correct = first / second
 
-  if given == correct:
-    print(f"{given} is the correct answer!")
-  else:
-    print(f"You're wrong. The correct answer is {correct}")
-    print("Game Over!")
-    exit()
+	given = Fraction(input(f"Your Answer: "))
+
+	if given == correct:
+		print(f"{given} is the correct answer!")
+	else:
+		print(f"You're wrong. The correct answer is {correct}")
+		print("Game Over!")
+		exit()
 
 print("You Win! You're good at this!")
 
